969fb1f38d7ba967a822b279a8a9d684e13cace8
Francisco Molina <frankmolinadev@gmail.com>
2022-04-25 21:09:19 -0300

    Agregados elementos del header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -6,11 +6,23 @@ const Header = () => {
         <>
             <header className='header'>
                 <div className='header-content-wrapper'>
+                    <div>
+                        <button>
+                            menu
+                        </button>
+                        <input title='Buscar' className='input-search' />
+                    </div>
+                    <div>
+                        <a href="/" className='header-logo'>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/2/28/Logo_La_Naci%C3%B3n.svg' alt='' />
+                        </a>
+                    </div>
 
-                    <a href="/" className='header-logo'>
-                        <img src='https://upload.wikimedia.org/wikipedia/commons/2/28/Logo_La_Naci%C3%B3n.svg' alt='' />
-                    </a>
-                    <a>subscribite</a>
+                    <div>
+                        <a href="/" >subscribite</a>
+                        <a href="/" >ingresar</a>
+                    </div>
+
                 </div>
             </header>
         </>
